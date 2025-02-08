139b55f355e41f015e2ddcb26b608dd656f479ea
Imran N. Emon <emon.genius@gmail.com>
2025-02-09 01:03:16 +0600

    [Added] CoolEscape: [CES002]
    1. Install library Djangorestframework, rest_framework_simplejwt, and drf-spectacular, python-decouple, and requests
    2. Added Logger to the project
    3. Added Configurations for Rest Framework, Simple JWT, and Spectacular and Logger
    4. Added Decouple for the environment variables
    5. git ignore file has been slightly changed.
    
    CES002
    On branch master
    Changes to be committed:
          new file:   .gitignore
          modified:   .idea/vcs.xml
          modified:   CoolEscape/settings.py
          new file:   commit_template.txt

--- CoolEscape/settings.py
+++ CoolEscape/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-df1#i&&q+n+k_d0q#pamwr#$-5m9a#=^py0f807abrq_y(0evq'
+SECRET_KEY = f"'{config('SECRET_KEY')}'"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -28,7 +30,16 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
+#Custom Applications
+CUSTOM_APPS = [
+]
+
+# install library
+INSTALLED_LIBRARIES = [
+    'rest_framework',
+    'drf_spectacular',
+    'rest_framework_simplejwt.token_blacklist',
+]
 
 INSTALLED_APPS = [
     'django.contrib.auth',
@@ -36,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    *CUSTOM_APPS,
+    *INSTALLED_LIBRARIES,
 ]
 
 MIDDLEWARE = [
@@ -105,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
@@ -121,3 +134,136 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Rest Framework Config
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+# Simple JWT Config
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    "VERIFYING_KEY": SECRET_KEY,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+    "JTI_CLAIM": "jti",
+
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(seconds=10),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(seconds=30),
+
+    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
+}
+
+
+# Rest Framework Config
+SPECTACULAR_SETTINGS = {
+    "TITLE": "CoolEscape API",
+    "DESCRIPTION": "An API that provides temperature-based travel recommendations and finds the coolest districts in Bangladesh.",
+    "VERSION": "1.0.0",
+
+    "SWAGGER_UI_SETTINGS": {
+        'deepLinking': True,
+        'persistAuthorization': True,
+        'docExpansion': 'none',
+    },
+    "REDOC_UI_SETTINGS": {
+        "theme": "light",
+    },
+    'DEFAULT_AUTO_SCHEMA': 'drf_spectacular.openapi.AutoSchema',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'SERVE_PUBLIC': True,
+    'USE_SESSION_AUTH': False,
+    'REDUCER': 'drf_spectacular.reducing.RouterDepthReducer',
+    'COMPONENT_SPLIT_REQUEST': True,
+    "SWAGGER_UI_DIST": "https://cdn.jsdelivr.net/npm/swagger-ui-dist@latest",
+    'DEFAULT_FIELD_INSPECTORS': [
+        'drf_spectacular.inspectors.CamelCaseJSONFilter',
+        'drf_spectacular.inspectors.InlineSerializerInspector',
+        'drf_spectacular.inspectors.RelatedFieldInspector',
+        'drf_spectacular.inspectors.ChoiceFieldInspector',
+        'drf_spectacular.inspectors.FileFieldInspector',
+        'drf_spectacular.inspectors.DictFieldInspector',
+        'drf_spectacular.inspectors.SimpleFieldInspector',
+        'drf_spectacular.inspectors.StringDefaultFieldInspector',
+    ],
+    "TAGS": [
+        {"name": "Coolest Districts", "description": "Find the coldest places to travel."},
+        {"name": "Travel Advice", "description": "Compare locations and get travel recommendations."},
+        {"name": "User Authentication", "description": "User authentication and authorization."},
+    ],
+}
+
+# Logger Config
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/debug.log'),
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'myapp': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
